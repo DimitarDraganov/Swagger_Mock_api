6d49ef9632d84d3d9055f2790a014954cde37ed7
DimitarDraganov <37122094+DimitarDraganov@users.noreply.github.com>
2021-03-09 15:38:55 +0200

    Initial commit

---
+++ TodoWebApi/Data/TodoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TodoWebApi.Models;
+
+namespace TodoWebApi.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext (DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoWebApi.Models.Todo> Todo { get; set; }
+    }
+}
